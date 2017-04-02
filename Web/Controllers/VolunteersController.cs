07e9ebbac3e682f24b9e3911bbc81ee90e10cbfc
saphraxaeris <stephan.elias@upr.edu>
2017-04-01 22:16:29 -0400

    boop

--- Web/Controllers/VolunteersController.cs
+++ Web/Controllers/VolunteersController.cs
@@ -10,7 +10,7 @@ namespace Web.Controllers
     {
         public ActionResult Index()
         {
-            return View ();
+            return View();
         }
     }
 }
--- Web/Scripts/navigation.js
+++ Web/Scripts/navigation.js
@@ -1,1 +1,28 @@
-﻿
+﻿var Navigation = function() {
+
+	var selectNavOption = function(cssClass) {
+		var button = $(cssClass);
+		console.log(button);
+        	button.attr('background', '#009688');
+        	button.attr('color', '#ffffff');
+        	button.find('a').attr('color', '#ffffff');
+	}; 
+
+	return {
+        InitHome: function () {
+        	selectNavOption('.home');
+        },
+        InitVolunteers: function () {
+            selectNavOption('.volunteers');
+        },
+        InitCommunities: function () {
+            selectNavOption('.communities');
+        },
+        InitProfile: function () {
+            selectNavOption('.profile');
+        },
+        InitEvents: function () {
+            selectNavOption('.events');
+        }
+    }
+}();
